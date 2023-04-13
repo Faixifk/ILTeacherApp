8e3994e135e389832fe7ac57dade0e06f07ab102
Faixifk <35093954+Faixifk@users.noreply.github.com>
2023-04-13 23:35:49 +0500

    Bluetooth based attendance.

--- app/src/main/java/com/example/intellilearnteacherapp/MyApp.kt
+++ app/src/main/java/com/example/intellilearnteacherapp/MyApp.kt
@@ -37,8 +37,8 @@ class MyApp : Application() {
         instance = this
 
         retrofit = Retrofit.Builder()
-        //    .baseUrl("http://10.0.2.2:8000/")
-            .baseUrl("http://192.168.100.4:8000/")
+            .baseUrl("http://10.0.2.2:8000/")
+        //    .baseUrl("http://192.168.100.4:8000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
